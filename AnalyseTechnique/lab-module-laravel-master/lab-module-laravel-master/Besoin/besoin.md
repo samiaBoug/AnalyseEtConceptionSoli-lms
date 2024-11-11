a61c2bcf7a33cdde5e330d6393d369091981f79f
Samiaboug <bougtoubsamia@gmail.com>
2024-11-11 10:10:56 +0000

    tuto reveal

--- AnalyseTechnique/lab-module-laravel-master/lab-module-laravel-master/Besoin/besoin.md
+++
@@ -1,3 +0,0 @@
-# Objectif de lab
-
-Comment diviser une grande application Laravel en module ?
--- AnalyseTechnique/lab-module-laravel-master/lab-module-laravel-master/Tuto1/Blog-tuto1/app/Modules/GestionCategories/Routes/web.php
+++
@@ -1,9 +0,0 @@
-<?php
-use Illuminate\Support\Facades\Route;
-use App\Modules\GestionCategories\Controllers\CategoryController;
-
-Route::prefix('categories')->group(function () {
-    Route::get('/', [CategoryController::class, 'index'])->name('categories.index');
-    Route::get('/create', [CategoryController::class, 'create'])->name('categories.create');
-    Route::post('/', [CategoryController::class, 'store'])->name('categories.store');
-});
